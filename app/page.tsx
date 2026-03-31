e59894f39be9a61c3b757c4740636991d1c4ad2d
Don <154335034+Steveston147@users.noreply.github.com>
2026-03-31 16:57:22 +0900

    Update text description for Shady Salama

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const coreFaculty = [
   {
     name: "Shady Salama",
     title: "Assistant Professor, College of Information Science and Engineering",
-    text: "Artificial intelligence, combinatorial optimization, simulation modeling, evolutionary algorithms, and intelligent manufacturing.",
+    text: "Artificial intelligence, simulation modeling, evolutionary algorithms, and combinatorial optimization.",
     image: "/salama.jpg",
   },
 ];
